de178bf0503d8135b9d828d01f49c2d7f6a74248
Eleph <hy104556222@gmail.com>
2025-12-19 21:34:47 +0800

    Optimize Rime'UI, add some phrase. Allow neovim insert Tab

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -2,7 +2,6 @@ local opt = vim.opt
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-
 opt.relativenumber = true
 opt.clipboard:append("unnamedplus")
 
@@ -10,7 +9,7 @@ opt.clipboard:append("unnamedplus")
 
 opt.shiftwidth = 4 -- Indentation
 opt.tabstop = 4 -- Tab looks like
-opt.expandtab = true -- Convert to space TvT
+-- opt.expandtab = true -- Convert to space TvT
 -- opt.smarttab = true -- Distinguish tab and indentation
 
 opt.scrolloff = 7
